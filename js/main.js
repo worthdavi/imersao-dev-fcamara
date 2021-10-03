3e82e3c1f8fb992649ea01b03679c4209438d90d
davi costa <worthdavi@gmail.com>
2021-10-02 22:57:07 -0300

    objetivo do projeto + atts

--- js/main.js
+++ js/main.js
@@ -5,10 +5,17 @@ const Agendar = () => {
     if(!data || data == "")
         return alert("A data deve estar preenchida.")
 
+    let dateTime = new Date(data)
+    let currentTime = new Date()
+    if (dateTime < currentTime)
+        return alert("A data deve ser à partir de hoje.")
+
+
     if(!estacao || estacao == "") 
         return alert("É necessário especificar a estação.")        
     else if (estacao <= 0)
         return alert("É necessário ser um número maior que zero.")    
 
-    alert(`Seu escritório foi agendado com sucesso para o dia ${data}, na estação ${estacao}!`)
+    let fullDate = `${dateTime.getDate() + 1}/${dateTime.getMonth() + 1}/${dateTime.getFullYear()}`
+    alert(`Seu escritório foi agendado com sucesso para o dia ${fullDate}, na estação ${estacao}!`)
 }
